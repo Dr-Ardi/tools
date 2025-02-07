21fe2b2f1bd33211d9138c3f58efa92d1a9dbd58
RD <rdl4bs@gmail.com>
2025-02-07 20:04:04 +0200

    uni tools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Design from './Design';
+import Uni from './Uni';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import content from './content.json'
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/design" element={<Design content={content.design} />} />
+        <Route path="/uni" element={<Uni content={content.uni} />} />
       </Routes>
     </Router>
   );
